54879068863acada22bab21dab320e6a52052519
Budina Eka Prasetia <budinaeka@gmail.com>
2024-10-16 14:38:50 +0700

    Update init.php

--- init.php
+++ init.php
@@ -10,8 +10,8 @@
             PDO::ATTR_EMULATE_PREPARES   => false
         ];
 
-        $dsn = "pgsql:host=webmap-webgis.k.aivencloud.com;dbname=webmap;port=27007";
-        $pdo = new PDO($dsn, 'avnadmin', 'AVNS_op0RATQwoxpw0qxo8t0', $opt);
+        $dsn = "pgsql:host=bep-webgis24-budinaeka-4c0a.l.aivencloud.com;dbname=defaultdb;port=27007";
+        $pdo = new PDO($dsn, 'avnadmin', 'AVNS_ttc_mQLi70bgnnc1W59', $opt);
 
     } catch(PDOException $e) {
         echo "Error: ".$e->getMessage();
